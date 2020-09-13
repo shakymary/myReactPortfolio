0171549d261ba6c4315866abb92f45c4d6fe63da
Shakila Marando <marandoshakila1@gmail.com>
2020-09-12 23:48:45 -0700

    Updates

---
+++ precache-manifest.6d322c594173094dd1444f17a9483110.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d614903e69bbfadd23d8b81d7e1dd25c",
+    "url": "/myReactPortfolio/index.html"
+  },
+  {
+    "revision": "05911c14d39da8a155cf",
+    "url": "/myReactPortfolio/static/css/main.5325f0c0.chunk.css"
+  },
+  {
+    "revision": "03b5e6023ce9f25fcf50",
+    "url": "/myReactPortfolio/static/js/2.47a884a5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/myReactPortfolio/static/js/2.47a884a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05911c14d39da8a155cf",
+    "url": "/myReactPortfolio/static/js/main.7e91716b.chunk.js"
+  },
+  {
+    "revision": "ddf51a2b978d2649f0e2",
+    "url": "/myReactPortfolio/static/js/runtime-main.e62baad7.js"
+  }
+]);
