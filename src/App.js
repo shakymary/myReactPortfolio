57126fc23dfc17ff2f84136212e9c31dd26e528f
Shakila Marando <marandoshakila1@gmail.com>
2020-09-17 15:26:36 -0700

    fixing routes

--- src/App.js
+++ src/App.js
@@ -11,28 +11,14 @@ function App() {
   return (
     <Router>
       <div className="App">
+        <Header />
         <Switch>
-          <Route path="/about">
-            <Header />
-            <About />
-            <Footer />
-          </Route>
-          <Route path="/work">
-            <Header />
-            <Work />
-            <Footer />
-          </Route>
-          <Route path="/contact">
-            <Header />
-            <Contact />
-            <Footer />
-          </Route>
-          <Route path="/">
-            <Header />
-            <HomeBody />
-            <Footer />
-          </Route>
+          <Route exact path="/about" component={About} />
+          <Route exact path="/work" component={Work} />
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/" component={HomeBody} />
         </Switch>
+        <Footer />
       </div>
     </Router>
   );
